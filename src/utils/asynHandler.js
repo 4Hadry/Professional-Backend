944469aac593f2a6a8ce10f156b61bef30a09b8b
4Hadry <chammar234@gmail.com>
2024-01-11 15:16:20 +0500

    All Setup Configured

--- src/utils/asynHandler.js
+++ src/utils/asynHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (reguestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(reguestHandler(req, res, next)).catch((err) => next(err));
   };
 };
