08e3fb57194c2991b32eb0feda8127d7fada3607
4Hadry <chammar234@gmail.com>
2024-01-17 12:34:23 +0500

    Add Subscriber Model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is Subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom "subscriber" is subscribing
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
