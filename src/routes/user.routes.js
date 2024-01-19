eb1dc5068514f7877e552b992e04e234655ed051
4Hadry <chammar234@gmail.com>
2024-01-19 15:22:14 +0500

    Add new Routes for USer Controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeAccountDetails,
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   logOutUser,
   loginUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImg,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,5 +37,17 @@ router.post("/login", loginUser);
 // secured routes
 router.post("/logout", verifyJWT, logOutUser);
 router.post("/refresh-token", refreshAccessToken);
+router.post("/change-pass", verifyJWT, changeCurrentPassword);
+router.get("/current-user", verifyJWT, getCurrentUser);
+router.patch("/update-acc", verifyJWT, changeAccountDetails);
+router.patch("/avatar", verifyJWT, upload.single("/avatar"), updateUserAvatar);
+router.patch(
+  "/cover-img",
+  verifyJWT,
+  upload.single("/coverImage"),
+  updateUserCoverImg
+);
+router.get("/c/:username", verifyJWT, getUserChannelProfile);
+router.get("/history", verifyJWT, getWatchHistory);
 
 export default router;
