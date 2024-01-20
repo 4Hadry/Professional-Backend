e42bdf41200d6e401f6c5c5f15964b19f8377d5c
4Hadry <chammar234@gmail.com>
2024-01-20 12:27:30 +0500

    Add comment, like, playlist and tweet models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,20 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const tweetSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
